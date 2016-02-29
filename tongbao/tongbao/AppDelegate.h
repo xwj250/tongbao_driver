74a0769907b24a00fd8b705219200f648196c848
xwj250 <1551717384@qq.com>
2016-02-29 17:24:37 +0800

    Ui

---
+++ tongbao/tongbao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tongbao
+//
+//  Created by 薛文进 on 16/2/26.
+//  Copyright © 2016年 薛文进. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
